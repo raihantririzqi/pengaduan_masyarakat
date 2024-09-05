506d7d3c509c35114cb21c50c92c252a739e759b
Xzura <raihantri995@gmail.com>
2024-09-05 19:48:15 +0700

    Update route.ts

--- src/app/api/login/mobile/route.ts
+++ src/app/api/login/mobile/route.ts
@@ -1,7 +1,27 @@
-import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = () => {
-    return NextResponse.json({
-        message: "Pengaduan telah dikirim",
+const prisma = new PrismaClient();
+export const POST = async (req: NextRequest) => {
+    const { username, password } = await req.json();
+
+    const masyarakat = await prisma.masyarakat.findFirst({
+        where: {
+            username,
+            password,
+        },
     })
-}
+
+    if (!masyarakat) {
+        return NextResponse.json({
+            messsage: "invalid credentials",
+        }, {
+            status: 401,
+        });
+    }
+
+    return NextResponse.json({
+        message: "Logged in successfully",
+        nik: masyarakat.nik
+    });
+};
